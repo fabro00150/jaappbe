12bebac4a5108272c8054ca135e08cacd3c8bc41
fabro00150 <richard.farinango0590@utc.edu.ec>
2025-12-27 21:43:53 -0500

    hosts

--- jaap_djbe/settings.py
+++ jaap_djbe/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.jaappbe.net", "jaappbe.net", "localhost:8005"]
+# ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
